ca243e2731edf7100f4ebb42e3200547d6339d8d
Aswin <73487313+aswin11234@users.noreply.github.com>
2023-07-05 00:14:38 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_a32.mk
+    $(LOCAL_DIR)/twrp_a32.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_a32-user \
-    omni_a32-userdebug \
-    omni_a32-eng
+    twrp_a32-user \
+    twrp_a32-userdebug \
+    twrp_a32-eng
